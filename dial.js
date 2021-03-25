93cb4237bebee73d50f48476ef36958e65735ea0
Nermin314 <nermin_imamovic10@hotmail.com>
2021-03-25 10:15:31 +0100

    Finish ARI app

--- dial.js
+++ dial.js
@@ -18,10 +18,12 @@ class ConferenceCalls{
         this.bridgeID=bridgeID;
     }
 
-     update(extensions,conferenceNumber,bridgeID){
-        this.extenList=extensions;
-        this.confNum=conferenceNumber;
-        this.bridgeID=bridgeID;
+    inputExtension(extension){
+        this.extenList.push(extension)
+    }
+
+    popExten(extension){
+        this.extenList=this.extenList.filter(e=>e !== extension);
     }
     print(){
         if(this.extenList)
@@ -31,7 +33,7 @@ class ConferenceCalls{
     }
 }
 let calls=[];
-
+var num=1;
 
 // handler for client being loaded
 function clientLoaded(err, client) {
@@ -49,38 +51,37 @@ rl.prompt();
 
 rl.on('line', function(line) {
 
-  let dial=/(^(dial)(\s[0-9]+)+$)/;
-  let enterConf=/(^(enter)\s[a-zA-Z]+\d+$)/;
-  
-  //  console.log(line.trim().match(dial));
-    if(line.trim().match(enterConf)){
-        //enter some conference with "enter conf1"
-        //calls.print();
-        console.log('entering conference');
-    }
+  let dial=/(^((\w+)?(dial))(\s[0-9]+)+$)/;
+  let enterConf=/(^(enter)\s[a-zA-Z]+\d+\s\d+$)/;
+  let conference=/((conf)\d+)/g;
 
+    if(line.trim().match(enterConf)){
+        let confNum=line.trim().match(conference)[0];
+        let extension=line.trim().match(/\d{3,6}/g)[0];
+       enterToConference(confNum,extension);
+    }
     else if(line.trim().match(dial)){
-    //"dial endpoint1 enpoint2 ...etc"
     let extensions=line.trim().match(/[0-9]+/g);
-    console.log('dial');
     originate(extensions);
   }
     else if(line.trim()==='listCalls'){
-    console.log('ListCalls');
-    calls.print();
+    if(!(calls.length)) {
+        console.log("There is no calls");
+    }
+    else {
+        for(var call of calls) {
+            call.print();
+        }}
   } else {
-        console.log('Usage: \n 1) dial endpoint1 endpoint2 endpoint3 etc \n 2) listCalls \n 3) enter ConferenceNumber');
+        console.log('Usage: \n 1) dial [endpoint1] [endpoint2] [endpoint3] ... etc \n 2) listCalls - For listing all calls \n 3) enter ConferenceNumber [Endpoint] \n [endpoint]-300,539,..');
     }
     rl.setPrompt('>');
     rl.prompt();
-    }).on('close', function() {
-  console.log('Have a great day!');
-  process.exit(0);
-});
-
+    });
   
   function originate(extensions){
 
+    var conf='conf'+num++;
     var bridge = client.Bridge();
     bridge.create({type: 'mixing'}, function(err, bridge) {
       if (err) {
@@ -88,8 +89,7 @@ rl.on('line', function(line) {
       }
     });
       console.log('Bridge created : %s :)', bridge.id);
-      console.log(extensions);
-      calls.push(new ConferenceCalls(extensions,"conf1",bridge.id));
+      calls.push(new ConferenceCalls(extensions,conf,bridge.id));
 
       //Creating and adding all channels to bridge
       for(var endpoint of extensions){
@@ -112,24 +112,59 @@ rl.on('line', function(line) {
       }
 
   }
+    function enterToConference(confNum,extension){
 
+      for(var call of calls){
+          if(call.confNum===confNum){
+              call.inputExtension(extension);
+              client.channels.create({
+                  app: 'dial-test',
+                  endpoint: 'PJSIP/' + extension
+              }, (err, channel) => {
+                  if (err) {
+                      throw err;
+                  }
+                  console.log("Created Channel %s",channel.name);
+                  client.bridges.get({bridgeId:call.bridgeID},function(err,bridge){
+                      console.log("Adding Channel %s to bridge %s",channel.name,bridge.id);
+                      bridge.addChannel({channel:channel.id},function(err) {
+                          if (err) {
+                              throw err;
+                          }
+                      });
+                  });
+                  client.channels.dial({channelId:channel.id});
+              });
+          }
+      }
+    }
     
     function stasisStart(event, channel) {
-
-        console.log(util.format(
-            'Channel %s has entered the application', channel.name));
-
+        console.log(util.format('Channel %s has entered the application', channel.name));
     }
 
     function stasisEnd(event, channel) {
-        //destroy bridges and update calls when someone hangup from conference To-DO
 
-        console.log(util.format(
-            'Channel %s has left the application', channel.name));
+        let pjsipExtensionPart=/\d{3,6}-/;
+        let extension=channel.name.match(pjsipExtensionPart)[0].replace('-','');
+        console.log(extension);
+        for (var call of calls){
+            if(call.extenList.includes(extension)){
+                call.popExten(extension);
+                if(!call.extenList.length){
+                    console.log("prazno");
+                    client.bridges.destroy(
+                        {bridgeId: call.bridgeID},
+                        function (err) {}
+                    );
+                    calls=calls.filter(e=>e.bridgeID !== call.bridgeID );
+                }
+            }
+
+        }
+        console.log(util.format('Channel %s has left the application', channel.name));
     }
-
     client.on('StasisStart', stasisStart);
     client.on('StasisEnd', stasisEnd);
-
     client.start('dial-test');
 }
